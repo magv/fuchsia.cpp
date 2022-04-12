05466171b025d018d4c75fe4a6ab445f5b6109e1
Vitaly Magerya <magv@tx97.net>
2022-04-12 15:25:19 +0200

    Use pkg-config in mkversion.sh, grop git

--- mkversion.sh
+++ mkversion.sh
@@ -1,11 +1,8 @@
-gitver() {
-    git -C "$1" describe --tags --abbrev=0 | sed '{s/^[^0-9.-]*//;s/-/./g}'
-}
-
 gitid() {
-    cidlong=$(git -C "$1" describe --always --abbrev=0 --match '')
-    cidshort=$(git -C "$1" describe --always --match '' --dirty=+)
-    date=$(git -C "$1" log -1 $cidlong --date=format:'%Y-%m-%d' --format=format:'%cd')
+    cidlong=$(git --git-dir "$1/.git" describe --always --abbrev=0 --match '' 2>/dev/null)
+    if [ $? -ne 0 ]; then return 1; fi
+    cidshort=$(git --git-dir "$1/.git" describe --always --abbrev=12 --match '' --dirty=+)
+    date=$(git --git-dir "$1/.git" log -1 $cidlong --date=format:'%Y-%m-%d' --format=format:'%cd')
     echo "commit $cidshort from $date"
 }
 
@@ -21,11 +18,18 @@ pkgid_dpkg() {
     printf "%s" "$info" | sed -n '/Version: /{s/^.* //;p}'
 }
 
+pkgid_pkgconf() {
+    info=$(pkg-config --modversion "$1" 2>/dev/null)
+    if [ $? -ne 0 ]; then return 1; fi
+    printf "%s" "$info" | head -1
+}
+
 pkgid() {
     local pkg
     for pkg in "$@"; do
         pkgid_rpm "$pkg" && return
         pkgid_dpkg "$pkg" && return
+        pkgid_pkgconf "$pkg" && return
     done
     echo "unknown"
 }
@@ -34,13 +38,13 @@ cat <<EOF
 static const char VERSION[] = R"(\
 Fuchsia, $(gitid .)
 Libraries:
-  GiNaC $(gitver "$GINAC"), $(gitid "$GINAC")
-  CLN $(gitver "$CLN"), $(gitid "$CLN")
+  GiNaC $(pkgid ginac libginac libginac-dev)
+  CLN $(pkgid cln libcln libcln-dev)
   GMP $(pkgid gmp libgmp-dev)
   glibc $(pkgid glibc libc6)
   libstdc++ $(pkgid libstdc++ libstdc++6)
 Compiler:
-  $($CXX --version | head -1)
+  $(${CXX:-c++} --version | head -1)
 Compressor:
   UPX $(pkgid upx upx-ucl)
 )";
